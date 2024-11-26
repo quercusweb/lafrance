9f520153b6d6a3e23bdd1e5db10d40d5ba2e8784
quercusweb <creationquercus@gmail.com>
2024-11-26 09:26:02 +0100

    Update config.mjs

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -26,7 +26,7 @@ export default defineConfig({
         collapsed: false,
         items: [
           { text: '⚖️ Indices', link: 'bourse/indices' },
-          { text: '📈 Actions', link: 'bourse/actions' },
+          { text: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chart-candlestick"><path d="M9 5v4"/><rect width="4" height="6" x="7" y="9" rx="1"/><path d="M9 15v2"/><path d="M17 3v2"/><rect width="4" height="8" x="15" y="5" rx="1"/><path d="M17 13v3"/><path d="M3 3v16a2 2 0 0 0 2 2h16"/></svg> Actions', link: 'bourse/actions' },
           { text: '💰 ETF', link: 'bourse/etf' },
           { text: '💶 Obligations', link: 'bourse/obligations' },
           { text: '🥇 Or', link: 'bourse/gold' }
