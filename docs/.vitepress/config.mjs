d00195378f2e8b71514fbb0d2d7acff8daf3c882
quercusweb <creationquercus@gmail.com>
2024-11-26 09:40:14 +0100

    Update config.mjs

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -29,7 +29,7 @@ export default defineConfig({
           { text: '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chart-candlestick"><path d="M9 5v4"/><rect width="4" height="6" x="7" y="9" rx="1"/><path d="M9 15v2"/><path d="M17 3v2"/><rect width="4" height="8" x="15" y="5" rx="1"/><path d="M17 13v3"/><path d="M3 3v16a2 2 0 0 0 2 2h16"/></svg>Actions', link: 'bourse/actions' },
           { text: '💰 ETF', link: 'bourse/etf' },
           { text: '💶 Obligations', link: 'bourse/obligations' },
-          { text: '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-shovel"><path d="M2 22v-5l5-5 5 5-5 5z"/><path d="M9.5 14.5 16 8"/><path d="m17 2 5 5-.5.5a3.53 3.53 0 0 1-5 0s0 0 0 0a3.53 3.53 0 0 1 0-5L17 2"/></svg>Or', link: 'bourse/gold' }
+          { text: '<Shovel :size="16" :stroke-width="1" />Or', link: 'bourse/gold' }
         ]
       },
       {
