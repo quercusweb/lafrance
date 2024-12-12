c1260e404e60dd50c10c2185e90315d940ac6d35
quercusweb <creationquercus@gmail.com>
2024-12-12 15:49:02 +0100

    Update config.mjs

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -79,7 +79,7 @@ export default defineConfig({
         collapsed: false,
         items: [
           { text: 'Euro', link: 'forex/euro' },
-          { text: 'Dollar', link: 'forex/dollar' }
+          { text: 'Dollar', link: 'forex/dollar', icon: { svg: '<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Dribbble</title><path d="M12...6.38z"/></svg>'} }
         ]
       },
       {
